94398df7ded9677963b95c6afa9a0a65d7865c5b
Alexis8796 <alekyu96@gmail.com>
2024-06-01 19:22:46 -0300

    Variables JS

---
+++ js/02.js
@@ -0,0 +1,26 @@
+// VAriables con Let
+
+let producto = 'Audifonos Gamer'; // Iniciar variable y darle un valor.
+
+let disponible; //Iniciamos variable pero sin valor.
+
+producto = true; //Reasignando el valord de la variable.
+
+disponible = true; 
+
+
+let producto1 = 'Computadora',
+    disponible1 = true,
+    categoria = 'Computadoras';
+
+// No se permite comenzar variables con numeros ni simbolos
+
+// Estilos para las variables
+let nombre_producto = 'Monitor HD';  // underscore
+let nombreProducto = 'Monitor HD'; // Camelcase
+let NombreProducto = 'Monitor HD'; // Pascal Case
+
+
+
+
+
---
+++ js/03.js
@@ -0,0 +1,24 @@
+// VAriables con Let
+
+const producto = 'Audifonos Gamer'; // Iniciar variable y darle un valor.
+
+const disponible; //No podemos iniciar una variable sin valor con CONST
+
+producto = true; //Reasignando el valord de la variable.
+
+disponible = true; //No se puede reasignar valores con CONST
+
+
+const producto1 = 'Computadora',
+    disponible1 = true,
+    categoria = 'Computadoras';
+
+// No se permite comenzar variables con numeros ni simbolos
+
+// Estilos para las variables
+const nombre_producto = 'Monitor HD';  // underscore
+const nombreProducto = 'Monitor HD'; // Camelcase
+const NombreProducto = 'Monitor HD'; // Pascal Case
+
+
+
